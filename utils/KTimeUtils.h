68b54969ed3e5850cd83db3c1b258a76afa0ed01
Timir <tkaria@capitalkpartners.com>
2012-02-09 22:33:41 +0000

    Changed time delta to milliseconds since that what it is returning!

--- utils/KTimeUtils.h
+++ utils/KTimeUtils.h
@@ -5,7 +5,7 @@
 #include <time.h>
 #include <ostream>
 
-unsigned long int timespec_delta_micros(const timespec& start, const timespec& end);
+unsigned long int timespec_delta_millis(const timespec& start, const timespec& end);
 timespec timespec_delta(const timespec& start, const timespec& end);
 
 std::ostream& operator<<(std::ostream& out, const timespec ts);
