5b1ba4e718559172f42f3e24c55cdf2bff76e751
Timir Karia <timir@capitalkpartners.com>
2012-03-28 15:55:56 +0000

    Abstract order interface class

---
+++ order-fix/NullOrderInterface.h
@@ -0,0 +1,71 @@
+#ifndef __NULLORDER_INTERFACE__
+#define __NULLORDER_INTERFACE__
+
+#include "OrderInterface.h"
+#include "logging.h"
+
+namespace capk
+{
+
+class NullOrderInterface : public virtual OrderInterface
+{
+	public: 
+		NullOrderInterface() {
+#ifdef LOG
+			pan::log_DEBUG("NullOrderInterface()");
+#endif
+		}
+
+		virtual ~NullOrderInterface() {
+#ifdef LOG
+			pan::log_DEBUG("~NullOrderInterface()");
+#endif
+		}
+
+		virtual void sndNewOrder(order_id_t& ClOrdID, 
+							const char* Symbol,
+							const side_t Side, 
+							const double OrderQty,
+							const short int OrdType,
+							const double Price,
+							const short int TimeInForce = 0,
+							const char* Account = NULL) {
+#ifdef LOG
+			pan::log_DEBUG("sndNewOrder()");
+			pan::log_DEBUG(pan::integer(Side), " ", Symbol, " ", pan::real(Price));
+#endif
+		}
+
+		virtual void sndCancelOrder(order_id_t& ClOrdID, 
+									order_id_t& OrigOrderID) {
+#ifdef LOG
+			pan::log_DEBUG("sndCancelOrder()");
+#endif
+		}
+
+		virtual void sndOrderCancelReplace() {
+#ifdef LOG
+			pan::log_DEBUG("sndOrderCancelReplace()");
+#endif
+		}
+
+		virtual void sndOrderStatus() {
+#ifdef LOG
+			pan::log_DEBUG("sndOrderStatus()");
+#endif
+		}
+
+		virtual void rcvExecutionReport() {
+#ifdef LOG
+			pan::log_DEBUG("rcvExecutionReport()");
+#endif
+		}
+	
+		virtual void rcvListStatus() {
+#ifdef LOG
+			pan::log_DEBUG("rcvListStatus()");
+#endif
+		}
+};
+};
+#endif // __NULLORDER_INTERFACE__
