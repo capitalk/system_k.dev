f4c9529c4abd38dab9ada328474fc354196075af
Timir Karia <timir@capitalkpartners.com>
2012-08-20 10:23:49 +0000

    Added missing files

---
+++ utils/config_server.h
@@ -0,0 +1,14 @@
+#ifndef CAPK_VENUE_CONFIG
+#define CAPK_VENUE_CONFIG
+
+#include "types.h" 
+#include "constants.h" 
+
+namespace capk
+{
+
+    const char REQ_CONFIG = 'C';
+    const char REQ_CONFIG_REFRESH = 'C';
+}
+
+#endif // CAPK_VENUE_GLOBALS
