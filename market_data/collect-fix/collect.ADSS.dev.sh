8778d85bdbb61f0864bd8a725cf36ec75186e04a
Timir <tkaria@capitalkpartners.com>
2013-02-07 17:06:27 +0000

    Removed screen start from sh scripts for DEV markets Removed static link from Makefile Changed CAPK.cfg to have correct port

--- market_data/collect-fix/collect.ADSS.dev.sh
+++ market_data/collect-fix/collect.ADSS.dev.sh
@@ -1,1 +1,1 @@
-screen -d -m -S ADSS.dev ./collect --c ./cfg/ADSS.dev.cfg --s ./cfg/symbols.cfg --o /home/timir/ADSS.dev
+./collect --c ./cfg/ADSS.dev.cfg --s ./cfg/symbols.cfg --o /home/timir/ADSS.dev
