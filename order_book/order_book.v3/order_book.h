b953034d469b08e8c24b8d3bc41de861917eddd1
Timir <tkaria@capitalkpartners.com>
2013-05-13 10:22:09 +0200

    Formatting changes to code

--- order_book/order_book.v3/order_book.h
+++ order_book/order_book.v3/order_book.h
@@ -15,24 +15,21 @@ const uint32_t MAX_ORDERS_PER_LEVEL = 10;
 const uint32_t MAX_ORDER_ID_LEN = 32;
 
 
-struct _order
-{
+struct _order {
     double quantity;
     char order_id[MAX_ORDER_ID_LEN];
 };
 typedef struct _order order;
 
-struct _priceLevel
-{
+struct _priceLevel {
     double _aggregate_qty;
-    int32_t _num_orders;
+    uint32_t _num_orders;
     double _price;
     order orders[MAX_ORDERS_PER_LEVEL];
 };
 typedef struct _priceLevel priceLevel;
 
-struct orderBook
-{
+struct orderBook {
     double init_price;
     int32_t num_levels;
     double one_way_offset_pct;
@@ -46,41 +43,34 @@ struct orderBook
 };
 typedef struct orderBook orderBook;
 
-int
-init_order_book(orderBook* order_book);
+int init_order_book(orderBook* order_book);
 
-orderBook*
-new_order_book(double init_price,
+orderBook* new_order_book(double init_price,
                double one_way_offset_pct,
                double min_tick_size);
 
 
-int
-addOrder(orderBook* ob,
+int addOrder(orderBook* ob,
+         const char* order_id,
          double price,
-         double quantity,
-         const char* order_id);
+         double quantity);
 
-int
-delOrder(orderBook* ob,
+int delOrder(orderBook* ob,
+         const char* order_id,
          double price,
-         double quantity,
-         const char* order_id);
+         double quantity);
 
-int
-modOrder(orderBook* ob,
-         double price,
-         double quantity,
-         const char* order_id);
+int modOrder(orderBook* ob,
+         const char* order_id,
+         double new_price,
+         double new_quantity);
 
 
-const priceLevel*
-bestBid(orderBook* ob) {
+const priceLevel* bestBid(orderBook* ob) {
     return NULL;
 }
 
-const priceLevel*
-bestOffer(orderBook* ob) {
+const priceLevel* bestOffer(orderBook* ob) {
     return NULL;
 }
 
